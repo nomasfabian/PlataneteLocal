c631695d2fd4f0710bb9b4365999745454faf2cd
nomasfabian <nomasfabian@gmail.com>
2021-06-13 16:27:31 -0500

    Primer version

---
+++ PlataneteViewer/PlataneteViewer/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace PlataneteViewer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.tbxUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel1.Location = new System.Drawing.Point(1, 32);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(615, 299);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(559, 3);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(45, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Ir..";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            this.btnBrowse.KeyDown += new System.Windows.Forms.KeyEventHandler(this.btnBrowse_KeyDown);
+            // 
+            // tbxUrl
+            // 
+            this.tbxUrl.Location = new System.Drawing.Point(1, 6);
+            this.tbxUrl.Name = "tbxUrl";
+            this.tbxUrl.Size = new System.Drawing.Size(552, 20);
+            this.tbxUrl.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.Location = new System.Drawing.Point(1, 334);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(615, 18);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Ultimo Mensaje:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 350);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbxUrl);
+            this.Controls.Add(this.btnBrowse);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox tbxUrl;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ PlataneteViewer/PlataneteViewer/Form1.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CefSharp;
+using CefSharp.WinForms;
+
+namespace PlataneteViewer
+{
+    public partial class Form1 : Form
+    {
+
+        ChromiumWebBrowser browser;
+        WindowsCrap keyManager;
+        public Form1()
+        {
+            InitializeComponent();
+            //Inicializamos Navegador
+            tbxUrl.Text = Application.StartupPath+"/index.html";
+            browser = new ChromiumWebBrowser(tbxUrl.Text);
+            panel1.Controls.Add(browser);
+            
+            // Instanciamos clase WindowsCrap =P
+            keyManager = new WindowsCrap();
+
+            //Instanciamos nuestro controlador asincrono
+            ManejadorEventosJs controladorAsincrono = new ManejadorEventosJs();
+            controladorAsincrono.mensajeRecibido += alRecibirMensaje;
+
+            //Asociamos nuesto objeto handler al navegador.
+            browser.JavascriptObjectRepository.Register("llamadaAsincrona", controladorAsincrono, true);
+
+            //Apagamos esta bandera para que otros hilos puedan modificar la ventana
+            CheckForIllegalCrossThreadCalls = false;
+        }
+
+        private void alRecibirMensaje(object sender, string e) {
+            label1.Text = "Ultimo mensaje: "+e;
+            switch(e)
+            {
+                case "UP":
+                    {
+                        WindowsCrap.Press(WindowsCrap.ScanCodeShort.UP);
+                        WindowsCrap.Release(WindowsCrap.ScanCodeShort.UP);
+                        break;
+                    }
+                case "DOWN":
+                    {
+                        WindowsCrap.Press(WindowsCrap.ScanCodeShort.DOWN);
+                        WindowsCrap.Release(WindowsCrap.ScanCodeShort.DOWN);
+                        break;
+                    }
+                case "LEFT":
+                    {
+                        WindowsCrap.Press(WindowsCrap.ScanCodeShort.LEFT);
+                        WindowsCrap.Release(WindowsCrap.ScanCodeShort.LEFT);
+                        break;
+                    }
+                case "RIGHT":
+                    {
+                        WindowsCrap.Press(WindowsCrap.ScanCodeShort.RIGHT);
+                        WindowsCrap.Release(WindowsCrap.ScanCodeShort.RIGHT);
+                        break;
+                    }
+            }
+   
+        }
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            //Abrimos la URL escrita
+            browser.Load(tbxUrl.Text);
+        }
+
+        private void btnBrowse_KeyDown(object sender, KeyEventArgs e)
+        {
+            //Detonamos el evento click en el boton
+            if (e.KeyCode == Keys.Enter)
+            {
+                btnBrowse_Click(this, new EventArgs());
+            }
+        }
+    }
+
+    public class ManejadorEventosJs {
+
+        public EventHandler<string> mensajeRecibido;
+        public void enviarMensaje(string mensaje)
+        {
+            if (mensajeRecibido != null)
+            {
+                mensajeRecibido(this,mensaje);
+            }
+        }
+    }
+}
